5c6bcb14e2485684859e07498263e75e11bdb047
Alireza Jafari <alirezaja1384@gmail.com>
2024-02-16 16:15:51 +0330

    routers: Swagger's APIKey auth method gets disabled in production

--- api/micro_media/routers/v1/__init__.py
+++ api/micro_media/routers/v1/__init__.py
@@ -5,12 +5,12 @@ from fastapi.security.api_key import APIKeyHeader
 from micro_media.settings import DEBUG
 from . import user, public, internal
 
+auth_deps = [Depends(HTTPBearer(auto_error=False))]
+if DEBUG:
+    auth_deps.append(Depends(APIKeyHeader(name="X-API-KEY", auto_error=False)))
+
 router = APIRouter(
-    # Enables swagger's authorize button
-    dependencies=[
-        Depends(HTTPBearer(auto_error=False)),
-        Depends(APIKeyHeader(name="X-API-KEY", auto_error=False)),
-    ]
+    dependencies=auth_deps  # Enables swagger's authorize button
 )
 
 router.include_router(user.router, prefix="/user", tags=["User"])
